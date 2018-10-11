e48e8504b1d27d19c4fda5a8790923080acac8bb
Myrza <Gapusta97@gmail.com>
2018-10-11 21:47:57 +0600

    Updated ExpressionUnitFactory.CustomFunctions,CustomUnaryOperators,CustomBinaryOperators attributes can be null so ExpressionUnitFactory has to handle this properly e.g. it has to consider only built-in functions if CustomFunctions is null.

--- src/main/java/edu/myrza/jmathexp/expression_unit/ExpressionUnitFactoryImpl.java
+++ src/main/java/edu/myrza/jmathexp/expression_unit/ExpressionUnitFactoryImpl.java
@@ -2,7 +2,6 @@ package edu.myrza.jmathexp.expression_unit;
 
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 class ExpressionUnitFactoryImpl implements ExpressionUnitFactory{
@@ -11,8 +10,7 @@ class ExpressionUnitFactoryImpl implements ExpressionUnitFactory{
     private Map<String,BinaryOperatorEU>    customBinaryOperators;
     private Map<String,UnaryOperatorEU>     customUnaryOperators;
 
-    //todo customFunction or custonBinaryOp or customUnOp can be null, We better read about Optional 'cos
-    //there are too many things that can be null    
+
     public ExpressionUnitFactoryImpl(Map<String,FunctionEU> customFunctions,
                                      Map<String,BinaryOperatorEU> customBinaryOperators,
                                      Map<String,UnaryOperatorEU> customUnaryOperators)
@@ -52,18 +50,30 @@ class ExpressionUnitFactoryImpl implements ExpressionUnitFactory{
 
         switch (type){
             case FUNCTION:{
-                Set<String> funcIds = new HashSet<>(customFunctions.keySet());
+                Set<String> funcIds = new HashSet<>();
+
+                if(customFunctions != null)
+                    funcIds.addAll(customFunctions.keySet());
                 funcIds.addAll(GlobalFunctions.getFunctionNames());
+
                 return funcIds;
             }
             case BINARY_OPERATOR:{
-                Set<String> binOpIds = new HashSet<>(customBinaryOperators.keySet());
+                Set<String> binOpIds = new HashSet<>();
+
+                if(customBinaryOperators != null)
+                    binOpIds.addAll(customBinaryOperators.keySet());
                 binOpIds.addAll(GlobalOperators.getBinaryOperatorNames());
+
                 return binOpIds;
             }
             case UNARY_OPERATOR:{
-                Set<String> unOpIds = new HashSet<>(customUnaryOperators.keySet());
+                Set<String> unOpIds = new HashSet<>();
+
+                if(customUnaryOperators != null)
+                    unOpIds.addAll(customUnaryOperators.keySet());
                 unOpIds.addAll(GlobalOperators.getUnaryOperatorNames());
+
                 return unOpIds;
             }
             default:{
@@ -75,34 +85,43 @@ class ExpressionUnitFactoryImpl implements ExpressionUnitFactory{
 
     private ExpressionUnit getLocalOrGlobalFunc(String id){
 
-        ExpressionUnit reqFunc = customFunctions.get(id);
-        if(reqFunc == null)
+        ExpressionUnit reqFunc = null;
+
+        if (customFunctions != null)
+            reqFunc = customFunctions.get(id);
+        if (reqFunc == null)
             reqFunc = GlobalFunctions.getFunction(id);
-        if(reqFunc == null)
-            throw new IllegalArgumentException("no such function with id " + id);
+        if (reqFunc == null)
+            throw new IllegalArgumentException("no such function with id : " + id);
 
         return reqFunc;
     }
 
     private ExpressionUnit getLocalOrGlobalBinOp(String id){
 
-        ExpressionUnit unaryOp = customUnaryOperators.get(id);
-        if(unaryOp == null)
-            unaryOp = GlobalOperators.getUnaryOperator(id);
-        if(unaryOp == null)
-            throw new IllegalArgumentException("no such unary operator with id : " + id);
+        ExpressionUnit binaryOp = null;
 
-        return unaryOp;
+        if (customBinaryOperators != null)
+            binaryOp = customBinaryOperators.get(id);
+        if (binaryOp == null)
+            binaryOp = GlobalOperators.getBinaryOperator(id);
+        if (binaryOp == null)
+            throw new IllegalArgumentException("no such binary operator with id : " + id);
+
+        return binaryOp;
     }
 
     private ExpressionUnit getLocalOrGlobalUnOp(String id){
 
-        ExpressionUnit binaryOp = customBinaryOperators.get(id);
-        if(binaryOp == null)
-            binaryOp = GlobalOperators.getBinaryOperator(id);
-        if(binaryOp == null)
-            throw new IllegalArgumentException("no such binary operator with id : " + id);
-        return binaryOp;
+        ExpressionUnit unaryOp = null;
+
+        if (customUnaryOperators != null)
+            unaryOp = customUnaryOperators.get(id);
+        if (unaryOp == null)
+            unaryOp = GlobalOperators.getUnaryOperator(id);
+        if (unaryOp == null)
+            throw new IllegalArgumentException("no such unary operator with id : " + id);
+        return unaryOp;
 
     }
 }

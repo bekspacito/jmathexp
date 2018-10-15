b13733619854aabdf9808f6a1864467b64848e12
Myrza <Gapusta97@gmail.com>
2018-10-15 22:10:16 +0600

    Added a Builder inside ExpressionUnitFactory.Now expression unit factories will be created only via builders

--- src/test/java/edu/myrza/jmathexp/test/TestExpressionUnitFactory.java
+++ src/test/java/edu/myrza/jmathexp/test/TestExpressionUnitFactory.java
@@ -1,92 +1,83 @@
 package edu.myrza.jmathexp.test;
 
-import edu.myrza.jmathexp.expression_unit.*;
-
-import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static edu.myrza.jmathexp.expression_unit.BuiltInOperators.*;
 import static org.junit.Assert.*;
 
+import edu.myrza.jmathexp.expression_unit.*;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.IntStream;
 
 public class TestExpressionUnitFactory{
 
-    static ExpressionUnitFactory factory;
+    @Test
+    public void testCustomFunc(){
 
-    @BeforeClass
-    public static void init(){
+        ExpressionUnitFactory factory = new ExpressionUnitFactoryImpl.Builder()
+                                                .addFunction("log",2,args -> Math.log(args[1])/Math.log(args[0]))
+                                                .build();
 
-        Map<String,Function> customFunctions = new HashMap<>();
-        customFunctions.put("arcsin",new Function("arcsin",1, args ->  Math.asin(args[0]) ));
 
-        Map<String,UnaryOperator> customUnaryOperators = new HashMap<>();
-        customUnaryOperators.put("!",new UnaryOperator("!",BuiltInOperators.POWER_PRECEDENCE + 1, arg -> {
+        ExpressionUnit log = factory.create(ExpUnitType.FUNCTION,"log");
+        assertNotNull(log);
+        assertEquals(4.0,log.evaluate(2,16),0.0);
 
-            if(arg % 1 != 0) throw new IllegalArgumentException("! operand must be integer...");
-            if(arg <= 0)     throw new IllegalArgumentException("fact defined only for values above zero....");
+    }
 
-            int intArg = (int)arg;
+    @Test
+    public void testCustomBinOp(){
 
-            return IntStream.rangeClosed(1,intArg)
-                            .reduce((i,j) -> i*j)
-                            .getAsInt();
+        ExpressionUnitFactory factory = new ExpressionUnitFactoryImpl.Builder()
+                .addBinaryOperator("<<",POWER_PRECEDENCE + 1,(op1,op2) -> {
 
-        }));
+                    int intOp1 = (int)op1;
+                    int intOp2 = (int)op2;
 
-        factory = new ExpressionUnitFactoryImpl(customFunctions,
-                                                null,
-                                                customUnaryOperators);
+                    return intOp1 << intOp2;
+
+                })
+                .build();
+
+        ExpressionUnit bitwiseShift = factory.create(ExpUnitType.BINARY_OPERATOR,"<<");
+        assertNotNull(bitwiseShift);
+        assertEquals(1024.0,bitwiseShift.evaluate(512,1),0.01);
+        assertEquals(2048.0,bitwiseShift.evaluate(512,2),0.01);
 
 
     }
 
-    /**
-     * Returns and executes factorial
-     * */
     @Test
-    public void testOne(){
+    public void testCustomUnaryOp(){
+
+        ExpressionUnitFactory factory = new ExpressionUnitFactoryImpl.Builder()
+                  .addUnaryOperator("!",POWER_PRECEDENCE + 1,arg -> {
+
+                      int intArg = (int)arg;
+
+                      return IntStream.rangeClosed(1,intArg)
+                                      .reduce((i,j) -> i*j)
+                                      .getAsInt();
+
+                  })
+                  .build();
 
         ExpressionUnit fact = factory.create(ExpUnitType.UNARY_OPERATOR,"!");
-
         assertNotNull(fact);
-
-        double result = fact.evaluate(5);
-
-        assertEquals(120.0,result,0.0);
+        assertEquals(120.0,fact.evaluate(5),0.001);
+        assertEquals(3628800.0,fact.evaluate(10),0.001);
 
     }
 
-    /**
-     * Returns and executes arcsin
-     * */
     @Test
-    public void testTwo(){
+    public void testBuiltInFunc(){
 
-        ExpressionUnit arcsin = factory.create(ExpUnitType.FUNCTION,"arcsin");
+        ExpressionUnitFactory factory = new ExpressionUnitFactoryImpl.Builder()
+                                                                     .build();
 
-        assertNotNull(arcsin);
-
-        double result = Math.toDegrees(arcsin.evaluate(1));
-
-        assertEquals(90.0,result,0.0);
-
-    }
-
-    /**
-     * Returns and executes addition
-     * */
-    @Test
-    public void testThree(){
-
-        ExpressionUnit add = factory.create(ExpUnitType.BINARY_OPERATOR,"+");
-
-        assertNotNull(add);
-
-        double result = add.evaluate(13,17);
-
-        assertEquals(30.0,result,0.0);
+        ExpressionUnit sqrt = factory.create(ExpUnitType.FUNCTION,"sqrt");
+        assertNotNull(sqrt);
+        assertEquals(8.0,sqrt.evaluate(64),0.0001);
 
     }
 
